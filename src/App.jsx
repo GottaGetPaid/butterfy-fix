a24025c2c324dc5b8555b2c4b26c2fd9e5410ec0
Hannah <hannahmukhtaar@gmail.com>
2024-12-05 15:22:51 -0800

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import List from "./pages/List.jsx";
+import Login from "./pages/Login.jsx"; 
 import Profile from "./pages/Profile.jsx"
+import Signup from "./pages/Signup.jsx"
+
 
 function App() {
   return (
     <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/list" element={<List />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/list" element={<List />} />
       </Routes>
     </Router>
   );
